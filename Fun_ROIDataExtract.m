248e1a0dccb741d64798a2b3a97e29be0d7dd0a4
xinos <xinyu@Xinyus-MBP.lan>
2020-06-24 23:34:33 +0800

    update ROI extraction code

--- Fun_ROIDataExtract.m
+++ Fun_ROIDataExtract.m
@@ -2,8 +2,13 @@ function ROIData = Fun_ROIDataExtract(ROImask,Datas)
 if ndims(Datas) == 2
     ROIData = mean(Datas(ROImask));
 else
-    nPixels = sum(ROImask(:));
+    MaskInds = find(ROImask);
     nFrames = size(Datas,3);
-    AllPixelData = reshape(Datas(repmat(ROImask,1,1,nFrames)),nPixels,nFrames);
-    ROIData = mean(AllPixelData);
+    ROIData = zeros(1,nFrames);
+    for cf = 1 : nFrames
+        cfData = squeeze(Datas(:,:,cf));
+        ROIData(1,cf) = mean(cfData(MaskInds)); %#ok<FNDSB>
+    end
+%     AllPixelData = reshape(Datas(repmat(ROImask,1,1,nFrames)),nPixels,nFrames);
+%     ROIData = mean(AllPixelData);
 end
