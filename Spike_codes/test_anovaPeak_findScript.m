003f5ed5412ed98d6dcc45bf095107ab85f2055d
xyza11808 <xyza11808@outlook.com>
2022-08-16 18:47:55 +0800

    add plots for anova trace plot

--- Spike_codes/test_anovaPeak_findScript.m
+++ Spike_codes/test_anovaPeak_findScript.m
@@ -2,23 +2,36 @@
 cA = 1;
 FactorStrs = {'Choice','Stim','BlockType'};
 % hf = figure('position',[100 100 1200 340]);
-figure;
-cf = 2;
+% figure;
+% cf = 2;
 nFactors = size(AllArea_anovaEVdatas,3);
 % AllArea_BTAnova_freqwise
 
-% for cf = 1 : nFactors
+for cf = 1 : nFactors
     cfRealData = AllArea_anovaEVdatas{cA,cf,1};
     cfThresData = AllArea_anovaEVdatas{cA,cf,2};
     
     cA_AvgTrace = mean(cfRealData,2);
-    cA_ThresAvg = median(cfThresData(:));
-    [pks,locs,w,p] = findpeaks(cA_AvgTrace,'MinPeakDistance',50,'MinPeakProminence',cA_ThresAvg,'MinPeakHeight',0.01,...
-        'Annotate','extents','WidthReference','halfheight');
+    cA_ThresAvg = mean(cfThresData,2);
+    
+    hf = figure('position',[100 100 380 260]);
+    hold on
+    plot(UnitCalWinTimes, cfRealData,'Color',[.7 .7 .7]);
+    plot(UnitCalWinTimes, cA_AvgTrace,'k','linewidth',1.5);
+    plot(UnitCalWinTimes, cA_ThresAvg,'c','linewidth',1,'linestyle','--');
+    line([0 0],[0 0.3],'Color','m','linewidth',1,'linestyle','--');
+    set(gca,'xlim',[-1.5 3.5])
+    xlabel('Times');
+    ylabel(FactorStrs{cf});
+    title('MOs EV');
+    saveas(hf,sprintf('%s MOs EV plot save',FactorStrs{cf}));
+    saveas(hf,sprintf('%s MOs EV plot save',FactorStrs{cf}),'png');
+%     [pks,locs,w,p] = findpeaks(cA_AvgTrace,'MinPeakDistance',50,'MinPeakProminence',cA_ThresAvg,'MinPeakHeight',0.01,...
+%         'Annotate','extents','WidthReference','halfheight');
     
     
 
-% end
+end
 
 %%
 close
